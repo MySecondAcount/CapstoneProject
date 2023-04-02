97673505ba65f090ae7330c8b0c707b864dda9ea
yazn <yaznyousef94@gmail.com>
2023-04-02 23:47:47 +0300

    fixed the token for the bootstrapping node

--- Bootstrap/src/main/java/com/example/bootstrap/NetworkManager.java
+++ Bootstrap/src/main/java/com/example/bootstrap/NetworkManager.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.logging.Logger;
+
 @Service
 public class NetworkManager {
     private static NetworkManager instance = null;
@@ -13,6 +15,9 @@ public class NetworkManager {
     private final String BOOTSTRAPPING_NODE_TOKEN = "@321bootstrappingNode123@";
     private final RestTemplate restTemplate;
     private final HttpHeaders headers;
+    private Logger logger = Logger.getLogger(NetworkManager.class.getName());
+
+
 
     private NetworkManager() {
         this.restTemplate = new RestTemplate();
@@ -29,12 +34,14 @@ public class NetworkManager {
     }
 
     public void removeExpiredToken(String username, String token, String workerName) {
+        logger.info("Removing expired token from " + workerName + "user name: " + username + " token: " + token);
         String url = "http://" + workerName + ":8081/api/removeAuthenticatedUser/" + username + "/" + token;
         HttpEntity<String> requestEntity = new HttpEntity<>("", headers);
         restTemplate.exchange(url, HttpMethod.DELETE, requestEntity, ApiResponse.class);
     }
 
     public void setUpWorkersNames() {
+        logger.info("Setting up workers names");
         for (int i = 1; i <= 3; i++) {
             String workerName = "worker" + i;
             String url = "http://" + workerName + ":8081/api/setCurrentWorkerName/" + workerName;
@@ -44,17 +51,17 @@ public class NetworkManager {
     }
 
     public void setFirstWorkerAsAffinity() {
+        logger.info("Setting worker1 as affinity");
         String url = "http://worker1:8081/api/setAffinity";
         HttpEntity<String> requestEntity = new HttpEntity<>("", headers);
         restTemplate.exchange(url, HttpMethod.GET, requestEntity, ApiResponse.class);
     }
 
     public void sendUserInfo(String username, String token, String workerName) {
+        logger.info("Sending user info to " + workerName + "user name: " + username + " token: " + token);
         String url = "http://" + workerName + ":8081/api/addAuthenticatedUser/" + username + "/" + token;
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("X-Username", "bootstrappingNode");
-        headers.set("X-Token", "bootstrappingNode");
         HttpEntity<String> requestEntity = new HttpEntity<>("", headers);
         restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class);
+        logger.info("User info sent to " + workerName + "user name: " + username + " token: " + token);
     }
 }
